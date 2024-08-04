4af0534a65b41df7f583a5a0b9778d02725fe26d
Amal Krishna <149648403+Amal-Krishna-TJ@users.noreply.github.com>
2024-08-05 01:23:27 +0530

    added feature sticky-top

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { HomeComponent } from './pages/home/home.component';
 import { ContactComponent } from './pages/contact/contact.component';
 import { AboutComponent } from './pages/about/about.component';
 import { ProductsComponent } from './pages/products/products.component';
+import { SingleComponent } from './pages/single/single.component';
 
 export const routes: Routes = [
     {path:"",component:HomeComponent},
     {path:"aboutus",component:AboutComponent},
     {path:"products",component:ProductsComponent},
     {path:"contactus",component:ContactComponent},
+    {path:"products/:id",component:SingleComponent}
 ];
--- src/app/pages/single/single.component.ts
+++ src/app/pages/single/single.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ApiService } from '../../api.service';
 
 @Component({
   selector: 'app-single',
@@ -8,5 +10,18 @@ import { Component } from '@angular/core';
   styleUrl: './single.component.css'
 })
 export class SingleComponent {
+  constructor(private routeId: ActivatedRoute, private api:ApiService){}
+  data:any=[]
 
+  ngOnInit(){
+
+    this.api.getProducts().subscribe((res:any)=>{
+      let id=this.routeId.snapshot.paramMap.get('id')
+      let response=res
+      let product=response.filter((e:any)=>e.id==id)
+      this.data=product[0]
+
+      
+    })
+  }
 }
